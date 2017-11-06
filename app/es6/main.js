12a97a62a76c49be4145098c2ead42aa6ae31ad5
drdynscript <drdynscript@gmail.com>
2017-11-06 19:52:20 +0100

    WIP

---
+++ app/es6/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import { YaHooWeatherService } from './services'
+
+class App {
+  constructor() {
+
+    console.log('Initialize the application sss.');
+  }
+
+  init = () => {
+    console.log('Initialize the application sss.');
+  }
+}
+window.addEventListener('load', () => {
+  const app = new App();
+  app.init();
+});
