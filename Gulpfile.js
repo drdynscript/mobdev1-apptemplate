9ad85f41ddc3a585e8ca91ca1d31440d8a6f663d
drdynscript <drdynscript@gmail.com>
2017-11-06 14:11:31 +0100

    Add Gulpfile and dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,3 +2,9 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var gulpSass    = require('gulp-sass');
 
+// Compile sass into CSS
+gulp.task('sass', function() {
+    return gulp.src('app/scss/*.scss')
+        .pipe(gulpSass())
+        .pipe(gulp.dest('app/css'));
+});
